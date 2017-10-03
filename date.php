ffc0b2f11015a3938bc6b20d2c2550115bda9c26
naveen basavaraj hottigimath <nh95@afsconnect1.njit.edu>
2017-10-03 14:14:19 -0400

    first date

--- date.php
+++ date.php
@@ -1,4 +1,6 @@
 <?php
-echo "the value of date is".date("Y-m-d")."<br>";
-echo str_replace("/","-");
+echo "The value of date:".$date("Y-m-d")."<br>";
+echo "The value of tar:".$date("Y/m/d")."<br>";
+$year = array("[0]"=>"2012","[1]"=>"396","[2]"=>"300","[3]"=>"2000","[4]"=>"1100","[5]"=>"1089");
+echo "The value of Year:".$year;
 ?>
